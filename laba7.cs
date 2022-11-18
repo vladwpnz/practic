fefe2d7e39312cda4e32a454478ade4ef20300b9
vladwpnz <90748608+vladwpnz@users.noreply.github.com>
2022-11-18 21:33:53 +0300

    Add files via upload
    Лабораторна робота номер 7, Спиридонов Владислав, ПД-24

---
+++ laba7.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> charsList = new List<char>();
+
+            Console.WriteLine("добавьте буквы в лист (просто нажимайте буквы), для продолжения нажмите Enter:");
+
+            ConsoleKey exit;
+            while ((exit = Console.ReadKey(true).Key) != ConsoleKey.Enter)
+            {
+                charsList.Add((char)exit);
+                Console.Write(exit.ToString().ToLower() + " ");
+            }
+
+
+            Console.WriteLine();
+
+            Console.Write("Выберите какой буквы хотите узнать индекс: ");
+
+            char a = (char)Console.ReadKey().Key;
+
+            Console.WriteLine();
+
+            Console.WriteLine("индексы:");
+
+
+            for (int i = 0; i < charsList.Count; i++) 
+
+                if (charsList[i] == a) Console.Write(i + " ");
+
+
+
+            char[] arr = charsList.ToArray();
+
+            Console.ReadKey(true);
+        }
+    }
+}
